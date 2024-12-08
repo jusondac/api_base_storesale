4255b7c5b08e844dc091ca2100ad6fadf5508639
Ari Yudianto <arbenyudianto@gmail.com>
2024-12-08 10:53:11 +0700

    change form for product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
+  accepts_nested_attributes_for :order_items
   has_many :products, through: :order_items
 
   validates :total_price, numericality: { greater_than_or_equal_to: 0 }
+
 end
